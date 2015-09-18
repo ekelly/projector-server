e9bb42a1524d88d1ea0461671ca0f13592cbf6f1
ekelly <eric.k.125@gmail.com>
2015-09-18 00:34:19 -0700

    Works!

--- server.js
+++ server.js
@@ -1,52 +1,105 @@
-var SerialPort = require("serialport").SerialPort
-var http = require('http');
+var SerialPort = require("serialport").SerialPort;
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+app.use(bodyParser.json());
+
+/*
+   { "payload": "ON" }
+*/
+
+const MAC_TTY = "/dev/tty.usbserial";
+const LINUX_TTY = "/dev/ttyUSB0";
+const SERIAL_NAME = MAC_TTY;
 
 const PORT=8080;
+const NULL=String.fromCharCode(0x0d);
 
-function writeToSerialPort(data) {
-  // Replace /dev/tty with the correct name
-  var serialPort = new SerialPort("/dev/tty", {
+const mapping = {
+  "power": "PWR",
+  "mute": "MUTE"
+};
+
+function createSerialPort(callback) {
+  var serialPort = new SerialPort(SERIAL_NAME, {
       baudRate: 9600,
       dataBits: 8,
       bufferSize: 255
   });
   serialPort.on("open", function() {
+    callback(serialPort);
+  });
+}
+
+function writeToSerialPort(data, error) {
+  createSerialPort(function(serialPort) {
     serialPort.write(data + "\r", function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
+      if (err) {
+        error(err);
+      }
     });
     serialPort.close(function() {});
   });
 }
 
-function handleRequest(request, response) {
-  var body = '';
-  request.setEncoding('utf8');
-
-  request.on('data', function(chunk) {
-    body += chunk;
+function getFromSerialPort(item, success, error) {
+  createSerialPort(function(serialPort) {
+    d = '';
+    serialPort.on("data", function(data) {
+      d += data;
+      if (data == ":") {
+        success(d.split("=")[1].split(NULL)[0]);
+        serialPort.close(function() {});
+      }
+    });
+    serialPort.write(item + "?\r", function(err, results) {
+      if (err) {
+        error(err);
+        serialPort.close(function() {});
+      }
+    });
   });
-
-  request.on('end', function() {
-    try {
-      writeToSerialPort("PWR OFF");
-      var data = JSON.parse(body);
-      var test = data["test"];
-      response.write(test);
-      console.log("received: " + data);
-    } catch (err) {
-      // bad json
-      response.statusCode = 400;
-      return response.end('error: ' + err.message);
-    }
-
-    response.end();
-  });
-
 }
 
-var server = http.createServer(handleRequest);
-server.listen(PORT, function() {
+app.get('/power', function(req, res) {
+  getFromSerialPort(mapping["power"], function(data) {
+    res.send(data);
+  });
+});
+
+app.post('/power', function(req, res) {
+  console.log(req.body);
+  var data = req.body.payload;
+  if (data === "ON" || data === "OFF") {
+    writeToSerialPort(mapping["power"] + " " + data, function(err) {
+      console.log(err);
+    });
+  } else {
+  }
+  res.send("");
+});
+
+app.get('/mute', function(req, res) {
+  getFromSerialPort(mapping["mute"], function(data) {
+    res.send(data);
+  }, function(err) {
+    console.log(err);
+  });
+});
+
+app.post('/mute', function(req, res) {
+  console.log(req.body);
+  var data = req.body.payload;
+  if (data === "ON" || data === "OFF") {
+    writeToSerialPort(mapping["mute"] + " " + data, function(err) {
+      console.log(err);
+    });
+  } else {
+  }
+  res.send("");
+});
+
+var server = app.listen(PORT, function() {
   // Callback triggered when successfully listening
-  console.log("Server listening on: http://localhost:%s\n", PORT);
+  console.log("Server listening on: http://localhost:%s\n", server.address().port);
 });
